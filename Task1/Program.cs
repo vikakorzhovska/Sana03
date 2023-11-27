f0fbb7aa5cde0747242657f94b4e3c643d6af1c4
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2023-11-27 18:53:42 +0200

    Compled 1 task

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+int count;
+Console.WriteLine("Введіть кількість елементів масиву: ");
+count = int.Parse(Console.ReadLine());
+Random random = new Random();
+int[] arr = new int[count];
+int sum = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = random.Next(-100, 101);
+    Console.Write($"{arr[i]} ");
+    if (arr[i] < 0)
+    {
+        sum += arr[i];
+    }
+}
+Console.WriteLine($"\nСума від'ємних елементів масиву: {sum}");
