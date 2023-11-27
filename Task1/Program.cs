3deb2e273299af23b792b4c5ad78c5557eb5472e
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2023-11-27 19:11:57 +0200

    Completed 3 task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -9,6 +9,8 @@ Random random = new Random();
 int[] arr = new int[count];
 int sum = 0;
 int min = arr[0];
+int max = arr[0];
+int maxindex = 0;
 for (int i = 0; i < arr.Length; i++)
 {
     arr[i] = random.Next(-100, 101);
@@ -21,6 +23,12 @@ for (int i = 0; i < arr.Length; i++)
     {
         min = arr[i];
     }
+    if (max < arr[i])
+    {
+        max = arr[i];
+        maxindex = i;  
+    }
 }
 Console.WriteLine($"\nСума від'ємних елементів масиву: {sum}");
-Console.WriteLine($"\nМінімальний елемент масиву: {min}");
+Console.WriteLine($"\nМінімальний елемент масиву: {min}"); 
+Console.WriteLine($"\nНомер максимального елементу: {maxindex}"); 
